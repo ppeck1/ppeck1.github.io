7278a5211c354e56e71dc57604c0eeb87183d45f
Paul Peck <Paultpeck@gmail.com>
2026-04-01 12:58:31 -0400

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://ppeck.me',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   output: 'static',
   build: {
     assets: 'assets'
